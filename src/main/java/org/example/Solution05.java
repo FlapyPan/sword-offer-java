ed3604afb4979b0c69210bc02d6400db6fb6df99
FlapyPan <flapypan@gmail.com>
2023-05-08 10:50:55 +0800

    第5题 2023-05-08

---
+++ src/main/java/org/example/Solution05.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ *  
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= s 的长度 <= 10000
+ */
+public class Solution05 {
+    public String replaceSpace(String s) {
+        if (s == null) return null;
+        StringBuilder builder = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == ' ') builder.append("%20");
+            else builder.append(c);
+        }
+        return builder.toString();
+    }
+
+    @Test
+    public void test() {
+        assert "We%20are%20happy.".equals(replaceSpace("We are happy."));
+    }
+}
