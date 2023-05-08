c0e6da8a5816ecdcea5c7bdc550bc7efc3df7593
FlapyPan <flapypan@gmail.com>
2023-05-08 10:50:46 +0800

    第4题 2023-05-07

---
+++ src/main/java/org/example/Solution04.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右 递增 的顺序排序，每一列都按照从上到下 递增 的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 现有矩阵 matrix 如下：
+ * <p>
+ * [
+ * [1,   4,  7, 11, 15],
+ * [2,   5,  8, 12, 19],
+ * [3,   6,  9, 16, 22],
+ * [10, 13, 14, 17, 24],
+ * [18, 21, 23, 26, 30]
+ * ]
+ * 给定 target = 5，返回 true。
+ * <p>
+ * 给定 target = 20，返回 false。
+ * 限制：
+ * <p>
+ * 0 <= n <= 1000
+ * <p>
+ * 0 <= m <= 1000
+ */
+public class Solution04 {
+
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null) return false;
+        int m = matrix.length;
+        if (m == 0) return false;
+        int n = matrix[0].length;
+        if (n == 0) return false;
+        if (matrix[0][0] > target || matrix[m - 1][n - 1] < target) return false;
+        int row = 0, col = n - 1;
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] > target) col -= 1;
+            else row += 1;
+        }
+        return false;
+    }
+
+    @Test
+    public void test() {
+        int[][] matrix = new int[][]{
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        assert findNumberIn2DArray(matrix, 5);
+        assert !findNumberIn2DArray(matrix, 20);
+
+    }
+}
