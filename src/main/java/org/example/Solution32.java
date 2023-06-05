a5f1530056489f742ccd2ece4ca2be431b399a15
FlapyPan <flapypan@gmail.com>
2023-06-05 11:28:37 +0800

    第32题 2023年6月5日

---
+++ src/main/java/org/example/Solution32.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.example.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+ */
+public class Solution32 {
+    public int[] levelOrder(TreeNode root) {
+        // 就是个层序遍历，秒了
+        if (root == null) return new int[0];
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+        }
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
